7ed85da39972c2137fdf3ee4c45860920c2162d0
liujames <f64951576@gmail.com>
2015-02-27 17:03:45 +0800

    eigen

---
+++ EigenValue/CMakeFiles/EigenValue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/EigenValue.dir/EigenValue.cpp.o"
+  "bin/x86_64/Release/EigenValue.pdb"
+  "bin/x86_64/Release/EigenValue"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/EigenValue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
